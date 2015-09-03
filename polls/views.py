905a5bfda94135f9f2eeb6527fd5cf947bbac92b
sonubudwal <sonubudwal20@gmail.com>
2015-09-03 15:12:47 -0700

    Adding new code

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views hefr
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello,World. You are at the polls index.")
