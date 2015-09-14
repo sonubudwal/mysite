42b44428c58c334910c40ec0c9bb698b40f7474c
sonubudwal <sonubudwal20@gmail.com>
2015-09-14 12:41:36 -0700

    commiting the project up to complete tutrial of part 3

--- polls/urls.py
+++ polls/urls.py
@@ -11,3 +11,4 @@ urlpatterns  = [
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
+
--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
-from django.shortcuts import render
+from django.template import RequestContext, loader
+from  .models import Questions
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 # Create your views hefr
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello,World. You are at the polls index.")
+    latest_question_list = Questions.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
 
 def detail(request,question_id):
-    return HttpResponse("You're looking at question %s"%question_id)
+    question = get_object_or_404(Questions, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request,question_id):
     response = "You're looking at the results of question %s."
